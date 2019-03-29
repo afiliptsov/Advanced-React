635ca6e52d1264afa6f9b91f7c107bf519d791c5
Anton Filiptsov <antonfiliptsov@BrushedBit-2.local>
2019-03-29 12:13:27 -0500

    routing added

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+const sell = props => (
+    <div>
+        <p>Hey</p>
+    </div>
+
+
+)
+export default sell;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+const sell = props => (
+    <div>
+        <p>Sell!</p>
+    </div>
+
+
+)
+export default sell;
